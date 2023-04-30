d0793dbe1d99932ef5139a66ad7c9f0ade0c6c94
madson812 <grantmadson@gmail.com>
2023-04-29 21:26:16 -0600

    Not able to get past main menu

--- src/test/groovy/towerDefense/FactorySpec.groovy
+++ src/test/groovy/towerDefense/FactorySpec.groovy
@@ -2,37 +2,54 @@ package towerDefense
 
 import com.almasb.fxgl.app.GameApplication
 import com.almasb.fxgl.entity.Entity
+import com.almasb.fxgl.entity.GameWorld
 import com.almasb.fxglgames.towerDefense.TowerComponent
 import com.almasb.fxglgames.towerDefense.TowerDefenseApp
 import spock.lang.Shared
 import spock.lang.Specification
 
+import java.sql.Time
+
 import static com.almasb.fxgl.dsl.FXGL.getGameWorld
 
 class FactorySpec extends Specification {
 
-//    @Shared
-//    GameApplication gameApp = new TowerDefenseApp()
-//
-//    def setupSpec() {
-//        // Start the FXGL engine before running tests
-//        gameApp.embeddedLaunch(TowerDefenseApp.main())
-//        wait(5000)
-//        gameApp.setLevel("first_level", "waveDataListA")
-//    }
-//
-//    def cleanupSpec() {
-//        // Shutdown the FXGL engine after all tests have completed
-//        gameApp.embeddedShutdown()
-//    }
-//
-//    def "test newTestEntity"() {
-//        given:
-//        boolean xkcd
-//        when: "fdjkl"
-//        List<Entity> towerEntities = getGameWorld().getEntitiesByComponent(TowerComponent.class)
-//        then: "fhdjkls"
-//        1 == 1
-//    }
+    @Shared
+    GameApplication gameApp = new TowerDefenseApp()
+
+    @Shared
+    GameWorld world
+
+    @Shared
+    def thread
+
+    synchronized void pause(){ wait(3000) }
+
+    def setupSpec() {
+        // Start the FXGL engine before running tests
+
+        thread = Thread.start {
+            gameApp.embeddedLaunch(TowerDefenseApp.main())
+        }
+        pause()
+        world = getGameWorld()
+        gameApp.setLevel("first_level", "waveDataListA")
+        pause()
+
+    }
+
+    def cleanupSpec() {
+        // Shutdown the FXGL engine after all tests have completed
+        gameApp.embeddedShutdown()
+    }
+
+    def "test newTestEntity"() {
+        given:
+        boolean xkcd
+        when: "fdjkl"
+        List<Entity> towerEntities = getGameWorld().getEntitiesByComponent(TowerComponent.class)
+        then: "fhdjkls"
+        1 == 1
+    }
 
 }
